fb5f85a3d42cfc8c3dd56e5ff3edfc98f44e909b
KwanJung98 <remf123@gmail.com>
2022-02-16 22:16:40 +0900

    todos state 정의 및 props 전달

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
+import { useState } from 'react';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: 'Coding Test 연습',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '인공지능 스터디 참여',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: 'React 프로젝트 참여',
+      checked: false,
+    },
+  ]);
+
   return (
-    <TodoTemplate> 
-      <TodoInsert/>
-      <TodoList/>
+    <TodoTemplate>
+      <TodoInsert />
+      <TodoList todos={todos}/>
     </TodoTemplate>
   );
 };
